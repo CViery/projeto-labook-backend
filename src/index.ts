1e7138bb9c225785b1e310da04dde24e6ece2473
Cristian Viery <viery.guimaraes01@gmail.com>
2023-08-24 20:14:25 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response} from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+import { userRouter } from './router/userRouter'
+import  dotenv  from 'dotenv'
 
-app.use(express.json());
-app.use(cors());
 
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003");
-});
+dotenv.config()
+const app = express()
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.send("Pong!");
-});
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+})
+
+app.use("/users", userRouter)
--- src/teste.js
+++
@@ -1,5 +0,0 @@
-const { log } = require("console")
-
-const lucros = 2489.8237
-const result = lucros.toFixed(3)
-console.log(result);
